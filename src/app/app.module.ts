77ead5c0aed31e6405080d4ea8a0eb7b2857edba
Diego Sierra <nirvana-ponal@hotmail.com>
2021-09-15 22:32:01 -0500

    creacion de cabecera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Vista1Component } from './componentes/vista1/vista1.component';
+import { HeadComponent } from './componentes/head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Vista1Component
+    Vista1Component,
+    HeadComponent
   ],
   imports: [
     BrowserModule
